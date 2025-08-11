2b96de6c8b0c3093e0f1daab2206db7749435364
kw <ru0dn7@gmail.com>
2025-08-11 17:47:39 +0900

    강의

--- react_basic/shop/src/App.js
+++ react_basic/shop/src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import data from "./data.js";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./routes/Detail.js";
+import axios from "axios";
 
 function App() {
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data); // json 데이터
+  let [clickCount, setClickCount] = useState(0); // 더보기 cnt
+  let [loading, setLoading] = useState(false); // 로딩 상태
+
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -14,15 +19,23 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
-            <Nav.Link href="#pricing">etc</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
 
       <Routes>
         {/* 메인페이지 */}
@@ -34,17 +47,91 @@ function App() {
 
               <div className="container">
                 <div className="row">
-                  {data.map(function (a, i) {
+                  {shoes.map(function (a, i) {
                     return <Card shoes={shoes[i]} i={i} key={i} />;
                   })}
                 </div>
               </div>
+
+              {/* 로딩 중일 때 스피너 */}
+              {loading && (
+                <div className="spinner-container">
+                  <div className="spinner"></div>
+                </div>
+              )}
+
+              {/* 버튼은 2번까지 표시 */}
+              {clickCount < 2 && !loading && (
+                <button
+                  onClick={() => {
+                    let url = "";
+                    if (clickCount === 0) {
+                      url = "https://codingapple1.github.io/shop/data2.json";
+                    } else if (clickCount === 1) {
+                      url = "https://codingapple1.github.io/shop/data3.json";
+                    }
+
+                    setLoading(true); // 로딩 시작
+                    axios
+                      .get(url)
+                      .then((결과) => {
+                        let copy = [...shoes, ...결과.data];
+
+                        // 로딩 끝을 1.5초 뒤로 미룸
+                        setTimeout(() => {
+                          setShoes(copy);
+
+                          setClickCount(clickCount + 1);
+                          // setLoading(false); // 로딩 끝
+
+                          setLoading(false);
+                        }, 2000);
+                      })
+                      .catch(() => {
+                        console.log("데이터를 불러오는 중 오류 발생");
+                        setLoading(false); // 로딩 끝
+                      });
+                  }}
+                >
+                  더보기
+                </button>
+              )}
             </>
           }
         />
         {/* 상세페이지 */}
-        <Route path="detail" element={<Detail />} />
+        <Route path="detail/:id" element={<Detail shoes={shoes} />} />
+        {/* about 페이지 */}
+        <Route path="about" element={<About />}>
+          <Route path="member" element={<div>멤버 입니다</div>} />
+          <Route path="location" element={<div>위지정보 입니다</div>} />
+        </Route>
+        <Route path="event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 1,000원 할인</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+
+        {/* 404페이지 */}
+        <Route path="*" element={<div>없는페이지입니다</div>} />
       </Routes>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보 페이지 입니다</h4>
+      <Outlet />
+    </div>
+  );
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>이벤트 페이지 입니다</h4>
+      <Outlet />
     </div>
   );
 }
--- react_basic/shop/src/routes/Detail.js
+++ react_basic/shop/src/routes/Detail.js
@@ -1,22 +1,78 @@
-function Detail() {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { Nav } from "react-bootstrap";
+
+function Detail(props) {
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function (x) {
+    return x.id == id;
+  });
+  let YllowBtn = styled.button`
+    background: ${(props) => props.bg};
+    color: ${(props) => (props.bg == "skyblue" ? "white" : "black")};
+    padding: 10px;
+  `;
+  let [count, setcount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let [탭, 탭변경] = useState(0);
+
+  // mount,update 시 실행된다
+  // 서버에서 데이터가져오기, 어려운 연산,타이머 장착 등 할때 사용
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
           <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            src={`https://codingapple1.github.io/shop/shoes${
+              찾은상품.id + 1
+            }.jpg`}
             width="100%"
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link onClick={()=>{탭변경(0)}} eventKey="link0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>{탭변경(1)}} eventKey="link1">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>{탭변경(2)}} eventKey="link2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <TabContent 탭={탭}/>
     </div>
   );
 }
 
+function TabContent({탭}) {
+  if (탭 == 0) {
+    return <div>내용0내용0내용0내용0</div>;
+  }
+  if (탭 == 1) {
+    return <div>내용1내용1내용1내용1내용1</div>;
+  }
+  if (탭 == 2) {
+    return <div>내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2</div>;
+  }
+}
 export default Detail;
